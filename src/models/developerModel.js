7dc323120c942e807a7d17d3d372c155fadf2d95
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 <rohinimirashe100@gmail.com>
2022-04-19 18:45:20 +0530

    Final commit

---
+++ src/models/developerModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+let ObjectId = mongoose.Schema.Types.ObjectId
+
+const developerSchema = new mongoose.Schema( {
+    name: String,
+    gender:{
+        type: String,
+        enum: ["male","female","other"]
+    } ,
+    batch_id:{
+        type:ObjectId,
+        ref:"Batch"
+    },
+
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('Developer', developerSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const authorController= require("../controllers/authorController")
-const publisherController = require('../controllers/publisherController')
-const bookController= require("../controllers/bookController");
+const batchController= require("../controllers/batchController")
+const developerController= require("../controllers/developerController");
 const { route } = require('express/lib/application');
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createAuthor", authorController.createAuthor  )
+router.post("/createBatch", batchController.createBatch )
 
-router.post('/createPublisher', publisherController.createPublisher)
+router.post("/createDeveloper", developerController.createDeveloper )
 
-router.post("/createBook", bookController.createBook  )
+router.post('/getDeveloperData', developerController.getDeveloperData)
 
-router.get('/get-all-books', bookController.fetchbooks)
-
-router.put('/books', bookController.updateBooks)
+router.post('/getDeveloperWithBatchDetails', developerController.getDeveloperWithBatchDetails)
 
 module.exports = router;
